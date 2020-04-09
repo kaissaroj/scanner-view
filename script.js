5fcbaec345308df2e3f296828948c0b892426a06
Saroj Subedi <kaissaroj@gmail.com>
2020-04-09 22:16:04 +0545

    update

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
-Quagga.init(
-  {
-    inputStream: {
-      name: "Live",
-      type: "LiveStream",
-      multiple: "true",
-      target: document.querySelector("#scanner-container"),
-      constraints: {
-        width: 640,
-        height: 480,
-        facingMode: "environment", // or user
-      },
-      locator: {
-        patchSize: "medium",
-        halfSample: false,
-      },
-    },
-    decoder: {
-      readers: [
-        {
-          format: "ean_reader",
-          config: {
-            supplements: ["ean_5_reader"],
-          },
+window.addEventListener("load", function () {
+  alert(!!Quagga);
+  Quagga.init(
+    {
+      inputStream: {
+        name: "Live",
+        type: "LiveStream",
+        multiple: "true",
+        target: document.querySelector("#scanner-container"),
+        constraints: {
+          width: 640,
+          height: 480,
+          facingMode: "environment", // or user
         },
-        "upc_reader",
-        "code_39_reader",
-        "code_39_vin_reader",
-      ],
+      },
+      decoder: {
+        readers: [
+          {
+            format: "ean_reader",
+            config: {
+              supplements: ["ean_5_reader"],
+            },
+          },
+          "upc_reader",
+          "code_39_reader",
+          "code_39_vin_reader",
+        ],
+      },
     },
-  },
-  function (err) {
-    if (err) {
-      alert(JSON.stringify(err));
+    function (err) {
+      if (err) {
+        alert(JSON.stringify(err));
+      }
+      alert("start scanning");
+      console.log("starting scanning");
+      Quagga.start();
     }
-    alert("start scanning");
-    console.log("starting scanning");
-    Quagga.start();
-  }
-);
+  );
 
-Quagga.onDetected(function (result) {
-  console.log(result.codeResult.code);
-  const code = result.codeResult.code;
-  alert(code);
+  Quagga.onDetected(function (result) {
+    console.log(result.codeResult.code);
+    const code = result.codeResult.code;
+    alert(code);
+  });
 });

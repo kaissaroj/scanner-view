531ad8ffe4f36dc555eeaa4b31a10fe9a1c98754
Saroj Subedi <kaissaroj@gmail.com>
2020-04-09 22:25:43 +0545

    update

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
 window.addEventListener("load", function () {
-  alert(!!Quagga);
-  Quagga.init(
-    {
-      inputStream: {
-        name: "Live",
-        type: "LiveStream",
-        multiple: "true",
-        target: document.querySelector("#scanner-container"),
-        constraints: {
-          width: 640,
-          height: 480,
-          facingMode: "environment", // or user
+  try {
+    const dom = document.querySelector("#scanner-container");
+    alert(!!dom);
+    Quagga.init(
+      {
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          multiple: "true",
+          target: document.querySelector("#scanner-container"),
+          constraints: {
+            width: 640,
+            height: 480,
+            facingMode: "environment", // or user
+          },
+        },
+        decoder: {
+          readers: [
+            {
+              format: "ean_reader",
+              config: {
+                supplements: ["ean_5_reader"],
+              },
+            },
+            "upc_reader",
+            "code_39_reader",
+            "code_39_vin_reader",
+          ],
         },
       },
-      decoder: {
-        readers: [
-          {
-            format: "ean_reader",
-            config: {
-              supplements: ["ean_5_reader"],
-            },
-          },
-          "upc_reader",
-          "code_39_reader",
-          "code_39_vin_reader",
-        ],
-      },
-    },
-    function (err) {
-      alert(JSON.stringify(err));
-      console.log("starting scanning");
-      Quagga.start();
-    }
-  );
+      function (err) {
+        alert(JSON.stringify(err));
+        console.log("starting scanning");
+        Quagga.start();
+      }
+    );
 
-  Quagga.onDetected(function (result) {
-    console.log(result.codeResult.code);
-    const code = result.codeResult.code;
-    alert(code);
-  });
+    Quagga.onDetected(function (result) {
+      console.log(result.codeResult.code);
+      const code = result.codeResult.code;
+      alert(code);
+    });
+  } catch (e) {
+    alert(JSON.stringify(e));
+  }
 });

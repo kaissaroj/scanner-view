6d69b14dc3d5ee92105fdfb2ab17ed435bd6f554
Saroj Subedi <kaissaroj@gmail.com>
2020-04-15 14:08:22 +0545

    updates

--- script.js
+++ script.js
@@ -1,92 +1,158 @@
-window.addEventListener("load", function () {
-  try {
-    const dom = document.querySelector("#scanner-container");
-if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
-  alert("enumerateDevices() not supported.");
-  return;
-}
+/**Quagga initialiser starts here*/
 
-var backCamID;
+$(function () {
+  var App = {
+    deviceId: null,
+    init: function () {
+      Quagga.init(this.state, function (err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        // App.attachListeners();
+        Quagga.start();
+      });
+    },
+    stop: function () {
+      Quagga.stop();
+    },
+    initCameraSelection: function () {
+      var streamLabel = Quagga.CameraAccess.getActiveStreamLabel();
 
-navigator.mediaDevices.enumerateDevices()
-.then(function(devices) {
-  devices.forEach(function(device) {
-    //alert( JSON.stringify(device) );
-    if( device.kind == "videoinput" && device.label.match(/back/) != null ){
-      //alert("Back found!");
-      backCamID = device.deviceId;
-    }
-  });
-})
-.catch(function(err) {
-  //alert(err.name + ": " + err.message);
-});
-
-if(typeof(backCamID)=="undefined"){
-  console.log("back camera not found.");
-}
-
-    Quagga.init(
-      {
-        inputStream: {
-          name: "Live",
-          type: "LiveStream",
-          multiple: "true",
-          target: document.querySelector("#scanner-container"),
-          constraints: {
-            width: 640,
-            height: 480,
-            facingMode: "environment", // or user
-          },
-        },
-        decoder: {
-          readers: [
-            {
-              format: "ean_reader",
-              config: {
-                supplements: ["ean_5_reader"],
-              },
-            },
-            "upc_reader",
-            "code_39_reader",
-            "code_39_vin_reader",
-          ],
-        },
-      },
-      initCameraSelection: function(){
-        var streamLabel = Quagga.CameraAccess.getActiveStreamLabel();
-
-        return Quagga.CameraAccess.enumerateVideoDevices()
-        .then(function(devices) {
-            function pruneText(text) {
-                return text.length > 30 ? text.substr(0, 30) : text;
-            }
-            var $deviceSelection = document.getElementById("deviceSelection");
-            while ($deviceSelection.firstChild) {
-                $deviceSelection.removeChild($deviceSelection.firstChild);
-            }
-            devices.forEach(function(device) {
-                var $option = document.createElement("option");
-                $option.value = device.deviceId || device.id;
-                $option.appendChild(document.createTextNode(pruneText(device.label || device.deviceId || device.id)));
-                $option.selected = streamLabel === device.label;
-                $deviceSelection.appendChild($option);
-            });
+      return Quagga.CameraAccess.enumerateVideoDevices().then(function (
+        devices
+      ) {
+        function pruneText(text) {
+          return text.length > 30 ? text.substr(0, 30) : text;
+        }
+        var $deviceSelection = document.getElementById("deviceSelection");
+        while ($deviceSelection.firstChild) {
+          $deviceSelection.removeChild($deviceSelection.firstChild);
+        }
+        devices.forEach(function (device) {
+          var $option = document.createElement("option");
+          $option.value = device.deviceId || device.id;
+          $option.appendChild(
+            document.createTextNode(
+              pruneText(device.label || device.deviceId || device.id)
+            )
+          );
+          $option.selected = streamLabel === device.label;
+          $deviceSelection.appendChild($option);
+        });
+      });
+    },
+    querySelectedReaders: function () {
+      return Array.prototype.slice
+        .call(document.querySelectorAll(".readers input[type=checkbox]"))
+        .filter(function (element) {
+          return !!element.checked;
+        })
+        .map(function (element) {
+          return element.getAttribute("name");
         });
     },
-      function (err) {
-        alert(JSON.stringify(err));
-        console.log("starting scanning");
-        Quagga.start();
-      }
-    )
+    _accessByPath: function (obj, path, val) {
+      var parts = path.split("."),
+        depth = parts.length,
+        setter = typeof val !== "undefined" ? true : false;
 
-    Quagga.onDetected(function (result) {
-      console.log(result.codeResult.code);
-      const code = result.codeResult.code;
-      alert(code);
-    });
-  } catch (e) {
-    alert(JSON.stringify(e));
+      return parts.reduce(function (o, key, i) {
+        if (setter && i + 1 === depth) {
+          if (typeof o[key] === "object" && typeof val === "object") {
+            Object.assign(o[key], val);
+          } else {
+            o[key] = val;
+          }
+        }
+        return key in o ? o[key] : {};
+      }, obj);
+    },
+    _convertNameToState: function (name) {
+      return name
+        .replace("_", ".")
+        .split("-")
+        .reduce(function (result, value) {
+          return result + value.charAt(0).toUpperCase() + value.substring(1);
+        });
+    },
+    setState: function (path, value) {
+      var self = this;
+
+      if (typeof self._accessByPath(self.inputMapper, path) === "function") {
+        value = self._accessByPath(self.inputMapper, path)(value);
+      }
+
+      self._accessByPath(self.state, path, value);
+      App.detachListeners();
+      Quagga.stop();
+      App.init();
+    },
+    state: {
+      inputStream: {
+        type: "LiveStream",
+        constraints: {
+          width: window.innerWidth,
+          height: window.innerHeight,
+          aspectRatio: { min: 1, max: 100 },
+          facingMode: "environment", // or user
+          deviceId: this.deviceId,
+        },
+      },
+      locator: {
+        patchSize: "large",
+        halfSample: true,
+      },
+      numOfWorkers: 4,
+      decoder: {
+        readers: [
+          {
+            format: "ean_reader",
+            config: {
+              supplements: ["ean_5_reader"],
+            },
+          },
+          "upc_reader",
+        ],
+      },
+      locate: true,
+      multiple: true,
+    },
+    lastResult: null,
+    customDecoders: null,
+  };
+
+  Quagga.onDetected(function (result) {
+    var code = result.codeResult.code;
+    if (activeCode !== code) {
+      codeReceived(code);
+    }
+  });
+  var activeCode = null;
+  var deviceId = null;
+  const getDeviceLists = async (toStart) => {
+    try {
+      let devices = await navigator.mediaDevices.enumerateDevices();
+      const backDevice = devices.filter(
+        (device) => device.kind == "videoinput"
+      );
+      deviceId = backDevice[backDevice.length - 1].deviceId;
+      App.deviceId = deviceId;
+      toStart == 1 && App.init();
+    } catch (e) {
+      alert(JSON.stringify(e));
+    }
+  };
+  function codeReceived(code) {
+    activeCode = code;
+    document.getElementById("id_num").innerText = code;
   }
+  getDeviceLists(0);
+  document.getElementById("scan_btn").addEventListener("click", function () {
+    if (!deviceId) {
+      getDeviceLists(1);
+    } else {
+      App.init();
+    }
+  });
 });

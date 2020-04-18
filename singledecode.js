db1c4222c13c8bca757b3dffba0857bc199752e3
Saroj Subedi <kaissaroj@gmail.com>
2020-04-18 13:49:14 +0545

    single decode image

---
+++ singledecode.js
@@ -0,0 +1,48 @@
+const decodeImage = (src, callback) => {
+  Quagga.decodeSingle(
+    {
+      size: 800,
+      locator: {
+        patchSize: "large",
+        halfSample: true,
+      },
+      numOfWorkers: 4,
+      decoder: {
+        readers: [
+          {
+            format: "ean_reader",
+            config: {
+              supplements: ["ean_5_reader"],
+            },
+          },
+          "upc_reader",
+          "code_128_reader",
+        ],
+      },
+      locate: true,
+      multiple: true,
+      readers: [
+        {
+          format: "ean_reader",
+          config: {
+            supplements: ["ean_5_reader"],
+          },
+        },
+        "upc_reader",
+        "code_128_reader",
+      ],
+      locate: true,
+      src: src,
+    },
+    function (result) {
+      callback(!!result.codeResult ? result.codeResult.code : null);
+    }
+  );
+};
+document.addEventListener("message", function (data) {
+  alert(data);
+  decodeImage(data, (code) => {
+    alert(code);
+    window.ReactNativeWebView.postMessage(code);
+  });
+});

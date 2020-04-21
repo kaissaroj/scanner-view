7acb6929527c7f5ee8231528cbd9068d935a871c
Saroj Subedi <kaissaroj@gmail.com>
2020-04-21 10:38:47 +0545

    updates9

--- singledecode.js
+++ singledecode.js
@@ -2,43 +2,43 @@
 $(function () {
   alert('web loaded')
   const decodeImage = async (src, type, callback) => {
-
-    const reader = type == 'ean-extended' ? [
-      {
-        format: "ean_reader",
-        config: {
-          supplements: ['ean_5_reader']
-        }
-      }
-    ] : 
-    [
-      "upc_reader",
-      "code_128_reader",
-    ];
+    callback('test');
+    // const reader = type == 'ean-extended' ? [
+    //   {
+    //     format: "ean_reader",
+    //     config: {
+    //       supplements: ['ean_5_reader']
+    //     }
+    //   }
+    // ] : 
+    // [
+    //   "upc_reader",
+    //   "code_128_reader",
+    // ];
   
   
-    await Quagga.decodeSingle(
-      {
-        inputStream: {size:1280},
-        locator: {patchSize:"medium",halfSample:true},
-        numOfWorkers:8,
-        decoder: {
-          readers:reader
-        },  
-        locate:true,
-        src: src,
-      },
-      function (result) {
+    // await Quagga.decodeSingle(
+    //   {
+    //     inputStream: {size:1280},
+    //     locator: {patchSize:"medium",halfSample:true},
+    //     numOfWorkers:8,
+    //     decoder: {
+    //       readers:reader
+    //     },  
+    //     locate:true,
+    //     src: src,
+    //   },
+    //   function (result) {
       
-        let code = null;
-        try {
-          code = result.codeResult.code;
-          alert(code)
-        } catch (e) {}
-        callback(code);
+    //     let code = null;
+    //     try {
+    //       code = result.codeResult.code;
+    //       alert(code)
+    //     } catch (e) {}
+    //     callback(code);
        
-      }
-    );
+    //   }
+    // );
   };
   document.addEventListener("message", function (event) {
     try {
